cecd6bf052e6427ef9b637e714fc978182bffbe7
mitabhay <leader.abhay002@gmail.com>
2024-04-05 14:36:37 +0530

    Post Model Created and migrated

--- core/admin.py
+++ core/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Profile
+from .models import Profile ,Post
 
 # Register your models here.
 admin.site.register(Profile)
-# admin.site.register(Post)
+admin.site.register(Post)
 # admin.site.register(LikePost)
 # admin.site.register(FollowersCount)
