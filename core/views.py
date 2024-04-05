e0bce3ebb66b5b296bd95ac0cc88ebfb651a4cad
mitabhay <leader.abhay002@gmail.com>
2024-04-05 14:52:42 +0530

    Like Post Functionality Added

--- core/views.py
+++ core/views.py
@@ -101,6 +101,29 @@ def settings(request):
         return redirect('settings')
     return render(request, 'setting.html', {'user_profile': user_profile})
 
+
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+        return redirect('/')
+
+
 def signup(request):
 
     if request.method == 'POST':
