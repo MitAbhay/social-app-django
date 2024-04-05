c86500306e76ef08873c0854bc66a89130ffe5bc
mitabhay <leader.abhay002@gmail.com>
2024-04-05 12:25:48 +0530

    Setting Page coomplete

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('settings', views.settings, name='settings'),
+    path('settings', views.settings, name='settings'),
     # path('upload', views.upload, name='upload'),
     # path('follow', views.follow, name='follow'),
     # path('search', views.search, name='search'),
